249c11b470196dbb605f715fa706bf4c0144b06f
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2021-03-31 13:57:34 +0200

    old custom functions

--- functions.R
+++ functions.R
@@ -2,3 +2,30 @@
 View.tweets <- function(x) {
   kable(x) %>% kable_styling("striped", full_width = TRUE)
 }
+
+#
+tweet_lookup <- function(dat, keyword, remove_retweets = TRUE) {
+  if (remove_retweets) {
+    dat <- dat %>% filter(!is_retweet)
+  }
+
+  results <-     dat %>%
+    filter(
+      str_detect(text_clean, regex(keyword, ignore_case = TRUE))
+    )
+  # Non-greedy matching
+  # regex(paste0(hashtag, "(\\W|$)"), ignore_case = TRUE)
+  
+
+  cat("n =", nrow(results))
+  return(results)
+}
+
+
+
+final %>% group_by(status_id) %>% 
+  summarise(
+    methods_used = paste(method, collapse = ", " )
+  )
+  
+
